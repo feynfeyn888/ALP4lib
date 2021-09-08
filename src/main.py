db13d9d0e65ec45c12124a74c7ca10e94e0d1e84
jamjamjam <50306919+feynfeyn888@users.noreply.github.com>
2021-09-08 19:46:33 +0900

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,87 +1,105 @@
 import numpy as np
 from ALP4 import *
 import time
-import datetime
 import os
-import cv2
-from my_module import ImgCap
-from my_module import Random_Binary
-
-today = datetime.date.today()
-now=datetime.now()
+import datetime
+from my_module import Random_Binary, Padding_Input_Matrix, ImgCap
 
 # Load the Vialux .dll
 DMD = ALP4(version = '4.3')
 # Initialize the device
 DMD.Initialize()
+img_Y,img_X=DMD.nSizeY,DMD.nSizeX
 
-# number of sampling
-pre_sampling=1000
-
-# exposure time(ms)
-exposure_time = 150.0
-
-##################################################
-
-# Input_Matirx
-Input_Matrix=Random_Binary(nx=DMD.nSizeY, ny=DMD.nSizeX, pre_sampling=1, seed=0)
-ImgBinaryRand = Input_Matrix*(2**8-1)
-
-directory_path='.\Input_Matrix\{}'.format(today)
-file_path='{}}\{}'.format(directory_path,now)
-# フォルダの有無確認
-if not os.path.exist(directory_path):
-    os.makedirs(directory_path)
-with open(file_path,mode='w') as f:
-    f.write(ImgBinaryRand)
-
-
-# img path
-img_directory_path='.\Img\{}'.format(today)
-# フォルダの有無確認
-if not os.path.exist(img_directory_path):
-    os.makedirs(img_directory_path)
+##############################
 
 # Binary amplitude image (0 or 1)
 bitDepth = 1
-ImgBlack = np.zeros([DMD.nSizeY,DMD.nSizeX])
 
-img_Y,img_X=ImgBinaryRand.shape
-pad_Y,pad_X=DMD.nSizeY-img_Y,DMD.nSizeX-img_X
+# Display Time on DMD(~10**7[μs]). 
+# display_time=t[s](t=1~10)
+display_time=1
 
-# padding
-ImgTarget_padding = cv2.copyMakeBorder(ImgBinaryRand,
-                                top=0,
-                                bottom=pad_Y,
-                                left=0,
-                                right=pad_X,
-                                borderType=cv2.BORDER_CONSTANT,
-                                value=(0,0,0))
+# 0:Random Binary, 1:Ones, 2:Zeros, 3:Partially Ones, 4:Shifted Ones
+Input_Matrix_Flag=4
+# up/down/left/right inversion
+Invertion_Flag=1
+
+## Input Matrix
+pre_sampling=1
+ny,nx=1000,1000
+
+# Camera Setup
+exposure_time = 150.0
+
+# Binary
+Input_Matrix=Random_Binary(ny=ny,nx=nx,pre_sampling=pre_sampling,seed=0,Input_Matrix_Flag=Input_Matrix_Flag)
+# print("Input_Matrix.shape:", Input_Matrix.shape)
+
+# reshape
+Reshape_Input_Matrix=Input_Matrix.reshape(pre_sampling,ny*nx).T
+
+# up/down/left/right inversion
+if Invertion_Flag==1:
+    Input_Matrix = Input_Matrix[:,::-1,::-1]
+
+ret=Padding_Input_Matrix(Input_Matrix,top=0,bottom=img_Y-ny,left=0,right=img_X-nx)
+print("ret.shape:",ret.shape)
+
+# # 0
+# Input_Matrix=Random_Binary(ny=ny,nx=nx,pre_sampling=pre_sampling,seed=0,Flag=2)
+# print("Input_Matrix.shape:", Input_Matrix.shape)
+# ret_=Padding_Input_Matrix(Input_Matrix,top=Y-ny,bottom=0,left=X-nx,right=0)
+
+# np.concatenate:reshape 1d_array
+# for _ in range(len(Input_Matrix))
+# imgSeq = np.concatenate([ret,ret_])
+imgSeq=ret
+print("ImgSeq",imgSeq)
+print("Img.shape:",imgSeq.shape)
+# Allocate the onboard memory for the image sequence
+
+DMD.SeqAlloc(nbImg = pre_sampling, bitDepth = bitDepth)
+    # nbImg:Number of images in the sequence(int)
+    # bitDepth:1(on-off)-8(256 pwm grayscale levels)(int)
+
+# Send the image sequence as a 1D list/array/numpy array
+DMD.SeqPut(imgData = imgSeq)
+
+# Set image rate to 50 Hz
+# illuminationTime[μs],pictureTime[μs]
+DMD.SetTiming(illuminationTime=display_time*10**6,pictureTime=display_time*10**6)
+
+# Save Input_Matrix by text.
+today = datetime.date.today()
+t_now=datetime.datetime.now().time()
+directory_path='.\Input_Matrix\{}'.format(today)
+file_path='{}\{}{}'.format(directory_path,t_now.hour,t_now.minute)
+# フォルダの有無確認
+if not os.path.exists(directory_path):
+    os.makedirs(directory_path)
+# save Input_Matrix
+np.savetxt(file_path,Reshape_Input_Matrix,fmt="%.0f")
+
+img_directory_path=directory_path='.\Img\{}\{}{}'.format(today,t_now.hour,t_now.minute)
+if not os.path.exists(img_directory_path):
+    os.makedirs(img_directory_path)
+
+# Run the sequence in an infinite loop
+DMD.Run()
+print("DMD RUN")
 
 for iter in range(pre_sampling):
-    # imgSeq  = np.concatenate([imgBlack.ravel(),imgWhite.ravel()])
-    imgSeq  = np.concatenate([ImgBlack.ravel(),ImgTarget_padding.ravel()])
+    print(iter+1)
+     # Capture
+    ImgCap(cap_id=iter, break_time=display_time, exposure_time=exposure_time, relative_path=img_directory_path)
 
-    # Allocate the onboard memory for the image sequence
-    DMD.SeqAlloc(nbImg = 2, bitDepth = bitDepth)
-    # Send the image sequence as a 1D list/array/numpy array
-    DMD.SeqPut(imgData = imgSeq)
+# Stop the sequence display
+DMD.Halt()
+print("DMD HALT")
 
-
-    # Set image rate to 50 Hz
-    DMD.SetTiming(pictureTime = 50000)
-
-    # Run the sequence in an infinite loop
-    DMD.Run()
-
-    time.sleep(1)
-
-    # Stop the sequence display
-    DMD.Halt()
-    # Free the sequence from the onboard memory
-    DMD.FreeSeq()
-    # De-allocate the device
-    DMD.Free()
-    
-    # Capture
-    ImgCap(cap_id=iter, break_time=1, exposure_time=exposure_time, relative_path=img_directory_path)
+# Free the sequence from the onboard memory
+DMD.FreeSeq()
+# De-allocate the device
+DMD.Free()
+print("DMD Free")
